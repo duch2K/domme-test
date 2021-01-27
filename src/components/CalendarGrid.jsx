a173ea0b2cb54054ac7a5aa49486178a6ae40e5f
Dulat Sarsenbek <sarsenbek.d@gmail.com>
2021-01-27 23:35:37 +0600

    bug fixes

--- src/components/CalendarGrid.jsx
+++ src/components/CalendarGrid.jsx
@@ -3,7 +3,7 @@ function CalendarGrid() {
     <div className="calendar-grid">
       {[...Array(35)].map((e, i) => (
         <div className="calendar-grid__item" key={i}>
-          <div className="calendar-grid__item-num">{i+1}</div>
+          <div className="calendar-grid__item-num">{i + 1}</div>
         </div>
       ))}
     </div>
