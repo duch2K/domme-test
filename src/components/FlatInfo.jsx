133f6727379d338aa62161d853e10f177481f688
Dulat Sarsenbek <sarsenbek.d@gmail.com>
2021-01-29 22:00:34 +0600

    added flat info

--- src/components/FlatInfo.jsx
+++ src/components/FlatInfo.jsx
@@ -1,10 +1,12 @@
 import userImg from '../img/user.jpg';
+import upIcon from '../img/up.svg';
 
 function FlatInfo() {
   return (
     <div className="flat-info">
       <h2>Квартира №134</h2>
-      <ul>
+
+      <ul className="flat-info__props">
         <li>Блок <span>2</span></li>
         <li>Подъезд <span>4</span></li>
         <li>Этаж <span>2</span></li>
@@ -19,6 +21,23 @@ function FlatInfo() {
           <span>Лиц. счет: 234255472</span>
         </div>
       </div>
+
+      <div className="flat-info__contacts">
+          <div className="flat-info__contacts-head">
+            <h3>Контактные данные</h3>
+            <img src={upIcon} alt="up"/>
+          </div>
+          <ul>
+            <li>
+              <span>E-mail</span>
+              <b>kovrogangster@mail.ru</b>
+            </li>
+            <li>
+              <span>Телефон</span>
+              <b>+7 707 111-22-33</b>
+            </li>
+          </ul>
+        </div>
     </div>
   );
 }
