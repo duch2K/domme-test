80278110aa34178ee6dd92444b7fc60292264e59
Dulat Sarsenbek <sarsenbek.d@gmail.com>
2021-01-29 17:53:45 +0600

    bug fixes

--- src/components/FlatInfo.jsx
+++ src/components/FlatInfo.jsx
@@ -3,11 +3,10 @@ function FlatInfo() {
     <div className="flat-info">
       <h2>Квартира №134</h2>
       <ul>
-        <li>Блок<span>2</span></li>
-        <li>Подъезд<span>4</span></li>
-        <li>Этаж<span>2</span></li>
+        <li>Блок <span>2</span></li>
+        <li>Подъезд <span>4</span></li>
+        <li>Этаж <span>2</span></li>
       </ul>
-      <div className="hr"></div>
     </div>
   );
 }
